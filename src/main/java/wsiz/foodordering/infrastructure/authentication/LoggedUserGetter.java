1f53859c6ab79f23e0f335b1b17403035c5d499a
Kamil Marnik <ksdekamil@gmail.com>
2021-12-17 22:10:02 +0100

    infrastructue classes, error handler

---
+++ src/main/java/wsiz/foodordering/infrastructure/authentication/LoggedUserGetter.java
@@ -0,0 +1,43 @@
+package wsiz.foodordering.infrastructure.authentication;
+
+import wsiz.foodordering.user.UserNotFound;
+import wsiz.foodordering.user.domain.CustomUserDetails;
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Optional;
+
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public final class LoggedUserGetter {
+
+  private LoggedUserGetter() {
+    throw new IllegalStateException("LoggedUserGetter is na utility class an can not be instantiated");
+  }
+
+  public static String getLoggedUsername() {
+    return Optional.ofNullable(SecurityContextHolder.getContext())
+        .map(SecurityContext::getAuthentication)
+        .map(Authentication::getName)
+        .orElseThrow(() -> new UsernameNotFoundException("Can not get currently logged in user"));
+  }
+
+  public static Long getLoggedUserId() {
+    return getLoggedInUser().getUserId();
+  }
+
+  public static boolean isAdmin() {
+    return getLoggedInUser().isAdmin();
+  }
+
+  private static CustomUserDetails getLoggedInUser() {
+    return Optional.ofNullable(SecurityContextHolder.getContext())
+        .map(SecurityContext::getAuthentication)
+        .map(authentication -> (CustomUserDetails) authentication.getPrincipal())
+        .orElseThrow(() -> new UserNotFound("Can not get currently logged in user"));
+  }
+
+}
